a57bbf443c205296c81e417980ff52bb8600c585
kd0tp <kpulut23@gmail.com>
2026-06-11 22:17:58 +0200

    Initial javafx Stage

--- src/main/java/astro/path/Main.java
+++ src/main/java/astro/path/Main.java
@@ -1,5 +1,30 @@
 package astro.path;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class main {
+public class Main extends Application{
+	
+	Button button = new Button("click here");
 
+	public static void main(String [] args) {
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.setTitle("First javafx Window");
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 600, 400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
 }
+
