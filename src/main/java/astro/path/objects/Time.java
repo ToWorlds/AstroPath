82d40b86cf2a4ee076aa3fb221490349498833f3
Nick <nicobuttgereit@gmail.com>
2026-07-02 21:57:11 +0200

    Rewrote Time Class to be used in simulated time steps

--- src/main/java/astro/path/objects/Time.java
+++ src/main/java/astro/path/objects/Time.java
@@ -1,15 +1,90 @@
 package astro.path.objects;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-
 public class Time {
+
+	long seconds;
+	
+	public Time() {
+		this.seconds = 0;
+		}
+	
+	public long getTotalSeconds() {
+		return seconds;
+	}
+	
+	public long getDisplaySeconds() {
+		return (seconds % 60);
+	}
+	
+	public long getTotalMinutes() {
+		return (seconds / 60);
+	}
+	
+	public long getDisplayMinutes() {
+		return ((getTotalMinutes()) % 60);
+	}
+	
+	public long getTotalHours() {
+		return (seconds / 3600);
+	}
+	
+	public long getDisplayHours() {
+		return (getTotalHours() % 24);
+	}
+	
+	public long getTotalDays() {
+		return (seconds / (3600 * 24));
+	}
+	
+	public long getDisplayDays() {
+		return (getTotalDays() % 365);
+	}
+	
+	public long getTotalYears() {
+		return (seconds / (3600 * 24 * 365));
+	}
+	
+	public void setTime (long seconds) {
+		this.seconds = seconds;
+	}
+	
+	public void setTime (long years, long days, long hours, long minutes, long seconds) {
+		long sum = 0;
+		sum += seconds;
+		sum += (minutes * 60);
+		sum += (hours * 3600);
+		sum += (days * 3600 * 24);
+		sum += (years * 3600 * 24 * 365);
+		this.seconds = sum;
+	}
+
+	public void addSeconds(long seconds) {
+		this.seconds += seconds;
+	}
+
+	public void addMinutes(long minutes) {
+		this.seconds += (minutes * 60);
+	}
+
+	public void addhours(long hours) {
+		this.seconds += (hours * 3600);
+	}
+
+	public void addDays(long days) {
+		this.seconds += (days * 3600 * 24);
+	}
+
+	public void addYears(long years) {
+		this.seconds += (years * 3600 * 24 * 365);
+	}
+	/*
 	long years;
 	long days;
 	long hours;
 	long minutes;
 	long seconds;
 	LocalDateTime StartTimeStamp;
+	
 	
 	public Time(){
 		this.years = 0;
@@ -79,4 +154,5 @@ public class Time {
 		
 		
 	}
+	*/
 }
